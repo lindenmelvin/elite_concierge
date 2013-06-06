a1b55816eb4a30d1ca13553c8a778d54dc12e47f
Linden Melvin <linden.melvin@gmail.com>
2013-06-05 21:32:32 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EliteConcierge::Application.config.secret_token = '00508d5e2831e8879846b1f452b06b633e600c6ac43ac904fba6e98ef251beffccaa72b35d5c13e13cb4788b79bb9bdf8643b38f03800e0aa7316db1621afe72'
