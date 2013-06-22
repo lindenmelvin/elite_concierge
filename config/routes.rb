cd660d0aafbf1cf8135b9ec4f541778906d68b23
Linden Melvin <linden.melvin@gmail.com>
2013-06-22 15:14:44 -0700

    fixing routes to make devise work for api

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ EliteConcierge::Application.routes.draw do
 
   resources :users
 
+  namespace :api do
+    devise_for :users, :path => '', :path_names => { :sign_in => 'login', :sign_out => 'logout' }
+    as :user do
+      get "/login" => "sessions#create"
+      get "/logout" => "sessions#destroy"
+    end
+  end
+  
   devise_for :users, :path => '', :path_names => { :sign_in => 'login', :sign_out => 'logout' }
   as :user do
     get "/login" => "sessions#new"
