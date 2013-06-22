749f18572f8bab7eae296aa213075725bda4bcbb
Linden Melvin <linden.melvin@gmail.com>
2013-06-22 14:11:54 -0700

    implementing devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_filter :authenticate_user!
   protect_from_forgery
   
+  def after_sign_out_path_for(resource)
+    return '/login'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 EliteConcierge::Application.routes.draw do
 
   resources :users
+
+  devise_for :users, :path => '', :path_names => { :sign_in => 'login', :sign_out => 'logout' }
+  as :user do
+    get "/login" => "sessions#new"
+    get "/logout" => "sessions#destroy"
+  end
   
   root :to => 'users#index'
 end
