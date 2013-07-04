9468d841b2943d18912f5ccfaf47304374473c26
Linden Melvin <linden.melvin@gmail.com>
2013-07-03 23:31:08 -0700

    dealing with routing after service request creation

--- app/controllers/api/service_requests_controller.rb
+++ app/controllers/api/service_requests_controller.rb
@@ -2,7 +2,11 @@ class Api::ServiceRequestsController < ApplicationController
   skip_before_filter :verify_authenticity_token
   
   def create
-    ServiceRequest.create!(params[:service_request])
+    if ServiceRequest.create!(params[:service_request])
+      render json: { success: true }
+    else
+      render json: { success: false }
+    end
   end
   
 end
