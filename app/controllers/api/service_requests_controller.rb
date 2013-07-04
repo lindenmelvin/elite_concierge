2383db124504c53896cef8efa1b0e801710548a5
Linden Melvin <linden.melvin@gmail.com>
2013-07-03 23:25:58 -0700

    adding attr_accessible to service request

--- app/controllers/api/service_requests_controller.rb
+++ app/controllers/api/service_requests_controller.rb
@@ -2,7 +2,7 @@ class Api::ServiceRequestsController < ApplicationController
   skip_before_filter :verify_authenticity_token
   
   def create
-    ServiceRequest.create!(params)
+    ServiceRequest.create!(params[:service_request])
   end
   
 end
