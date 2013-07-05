0aca658267599e435fd82587f23b07098033aba6
Linden Melvin <linden.melvin@gmail.com>
2013-07-04 18:01:06 -0700

    adding throw for testing

--- app/controllers/api/service_requests_controller.rb
+++ app/controllers/api/service_requests_controller.rb
@@ -2,6 +2,7 @@ class Api::ServiceRequestsController < ApplicationController
   skip_before_filter :verify_authenticity_token
   
   def create
+    throw current_user
     params[:service_request].merge!(:user_id => current_user)
     if ServiceRequest.create!(params[:service_request])
       render json: { success: true }
